9a04f18c96f72461510cd2f81525cf74965f5c86
Leandro Barbosa <leandrobar93@gmail.com>
2016-11-14 16:36:59 -0200

    add dot and angle from vector

--- algebralinear/vector.py
+++ algebralinear/vector.py
@@ -1,6 +1,8 @@
 # coding: utf-8
+import operator
 from array import array
-from math import sqrt
+from math import sqrt, pi, acos
+from functools import reduce
 
 
 class Vector(object):
@@ -39,6 +41,24 @@ class Vector(object):
     def __mag__(self):
         return round(sqrt(sum(pow(float(i), 2) for i in self)), 3)
 
-    def __normalize__(self):
+    def __normalized__(self):
         mag = 1 / round(sqrt(sum(pow(float(i), 2) for i in self)), 3)
         return Vector([round(mag * x, 3) for x in self])
+
+    def dot(self, vector):
+        return round(reduce(operator.add,
+                            tuple(map(lambda x, y: x*y, self, vector))), 3)
+
+    def angle_with(self, vector, in_degrees=False):
+        try:
+            u1 = self.__normalized__()
+            u2 = vector.__normalized__()
+            angle_in_radians = acos(u1.dot(u2))
+
+            if in_degrees:
+                degrees_per_radian = 180. / pi
+                return round(angle_in_radians * degrees_per_radian, 3)
+            else:
+                return round(angle_in_radians, 3)
+        except Exception as e:
+            raise e
--- tests/tests_vector.py
+++ tests/tests_vector.py
@@ -18,7 +18,35 @@ class VectorTestCase(TestCase):
 
         self.assertEqual(response, Vector([-0.329, -5.012]))
 
-    def test_multi_vector(self):
+    def test_dot_vector(self):
+        v1 = Vector([7.887, 4.138])
+        v2 = Vector([-8.802, 6.776])
+
+        self.assertEqual(v1.dot(v2), -41.382)
+
+    def test_other_dot_vector(self):
+        v1 = Vector([-5.955, -4.904, -1.874])
+        v2 = Vector([-4.496, -8.755, 7.103])
+
+        self.assertEqual(v1.dot(v2), 56.397)
+
+    def test_radius_vector(self):
+        v1 = Vector([3.183, -7.627])
+        v2 = Vector([-2.668, 5.319])
+
+        response = v1.angle_with(v2)
+
+        self.assertEqual(response, 3.078)
+
+    def test_angle_vector(self):
+        v1 = Vector([7.35, 0.221, 5.188])
+        v2 = Vector([2.751, 8.259, 3.985])
+
+        response = v1.angle_with(v2, in_degrees=True)
+
+        self.assertEqual(response, 60.264)
+
+    def test_escalar_vector(self):
         response = self.v1 * 7.41
         self.assertEqual(response, Vector([12.382, -7.499, -2.356]))
 
@@ -35,9 +63,9 @@ class VectorTestCase(TestCase):
     def test_normalization_vector(self):
         v1 = Vector([1.996, 3.108, -4.554])
 
-        self.assertEqual(v1.__normalize__(), Vector([0.34, 0.53, -0.777]))
+        self.assertEqual(v1.__normalized__(), Vector([0.34, 0.53, -0.777]))
 
     def test_normalization_other_quest(self):
         v1 = Vector([5.581, -2.136])
 
-        self.assertEqual(v1.__normalize__(), Vector([0.934, -0.357]))
+        self.assertEqual(v1.__normalized__(), Vector([0.934, -0.357]))
